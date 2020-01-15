fb7e62471862356e6ed840fca838770e705680c0
hermanwongkm <herman_wong_km@outlook.com>
2020-01-15 21:52:51 +0800

    add dotenv

--- models/index.js
+++ models/index.js
@@ -1,4 +1,7 @@
 "use strict";
+require("dotenv").config();
+console.log(process.env.POSTGRES_DB);
+
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
@@ -8,17 +11,22 @@ const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
+if (false) {
+  //config.use_env_variable
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
+    process.env.POSTGRES_DB,
+    process.env.POSTGRES_USER,
+    process.env.POSTGRES_PASSWORD,
+    {
+      host: process.env.POSTGRES_HOST,
+      dialect: "postgres"
+    }
   );
 }
 
+fs.readdirSyn;
 fs.readdirSync(__dirname)
   .filter(file => {
     return (
